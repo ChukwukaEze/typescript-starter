863be1b2adc542494d2c2366ceeece21a05bdbf1
ChukwukaEze <63220512+ChukwukaEze@users.noreply.github.com>
2023-12-06 05:06:05 +0100

    App Update

---
+++ src/employees/employees.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '../database/database.service';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class EmployeesService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createEmployeeDto: Prisma.EmployeeCreateInput) {
+    console.log({ createEmployeeDto });
+    return this.databaseService.employee.create({
+      data: createEmployeeDto,
+    });
+  }
+
+  async findAll() {
+    return this.databaseService.employee.findMany();
+  }
+
+  async findOne(id: number) {
+    return `This action returns a #${id} employee`;
+  }
+
+  async update(id: number, updateEmployeeDto: Prisma.EmployeeCreateInput) {
+    console.log({ updateEmployeeDto });
+    return `This action updates a #${id} employee`;
+  }
+
+  async remove(id: number) {
+    return `This action removes a #${id} employee`;
+  }
+}
